96f9284326d98e9adf84e183110395bad4e3153f
guptarashi1703-coder <guptarashi1703@gmail.com>
2026-09-09 12:46:51 +0530

    Time: 0 ms (100%), Space: 50.3 MB (27.48%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int l=0;
+        int r=citations.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(citations[mid]>=citations.length-mid)
+            r=mid-1;
+            else
+            l=mid+1;
+        }return citations.length-l;
+    }
+}
