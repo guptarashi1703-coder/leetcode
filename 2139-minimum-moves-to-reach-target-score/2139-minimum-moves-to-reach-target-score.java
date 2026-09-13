1c2cbb18ef0ca288db0a185675e5f93149427d81
guptarashi1703-coder <guptarashi1703@gmail.com>
2026-09-14 01:03:41 +0530

    Time: 0 ms (100%), Space: 42.3 MB (44.17%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+     int c=0;
+     while(target>1 && maxDoubles>0){
+        if(target%2==0){
+            target/=2;
+            maxDoubles--;
+        }else{
+            target-=1;
+        }c++;
+     }return c+target-1;
+    }
+}
